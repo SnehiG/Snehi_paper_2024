fc5bc5297b51ac8853daa92706448032e14ee2cd
KarenGoncalves <cris.kgs@gmail.com>
2024-02-16 08:43:23 -0500

    corrected script to separate metadata so that the files have species names in capslock

---
+++ scripts/download_rawdata.sh
@@ -0,0 +1,4 @@
+# Download file from SRA using sra-toolkit
+## Arabidopsis ##
+mkdir -p RAW_DATA/Arabidopsis
+cut metadata
--- scripts/separate_metadata_by_species.sh
+++ scripts/separate_metadata_by_species.sh
@@ -2,9 +2,9 @@
 awk -F '\t' -v OFS='\t' 'NR == 1 {print} NR > 1\
  && $6 ~ /P.+/ {print}'\
  metadata/PRJNA1072118.tsv >\
- metadata/PRJNA1072118_poplar.tsv
+ metadata/PRJNA1072118_Poplar.tsv
 
 awk -F '\t' -v OFS='\t' 'NR == 1 {print} NR > 1\
  && $6 ~ /^[AC].+/ {print}'\
  metadata/PRJNA1072118.tsv >\
- metadata/PRJNA1072118_arabidopsis.tsv
+ metadata/PRJNA1072118_Arabidopsis.tsv
