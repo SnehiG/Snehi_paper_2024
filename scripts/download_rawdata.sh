ffd28bbc2ebf8368c706ee1811be3d621c77981d
KarenGoncalves <cris.kgs@gmail.com>
2024-02-16 08:46:34 -0500

    script for downloading SRA files and separate by species

--- scripts/download_rawdata.sh
+++ scripts/download_rawdata.sh
@@ -1,4 +1,12 @@
 # Download file from SRA using sra-toolkit
 ## Arabidopsis ##
-mkdir -p RAW_DATA/Arabidopsis
-cut metadata
+for species in Arabidopsis Poplar; do
+ mkdir -p RAW_DATA/$species
+ cut -f 1 metadata/PRJNA1072118_$species.tsv |
+  while read accession; do
+   fasterq-dump --seq-defline '@$sn[_$rn]/$ri'\
+    --split-files $accession\
+    --outdir RAW_DATA/$species;
+ done
+done
+
