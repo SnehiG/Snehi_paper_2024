86fe5e0e902f826ad3b99405931af6bde11ba31f
KarenGoncalves <cris.kgs@gmail.com>
2024-02-16 08:55:52 -0500

    added creation of links to the script that download the raw data

--- scripts/download_rawdata.sh
+++ scripts/download_rawdata.sh
@@ -1,12 +1,17 @@
 # Download file from SRA using sra-toolkit
 ## Arabidopsis ##
 for species in Arabidopsis Poplar; do
- mkdir -p RAW_DATA/$species
- cut -f 1 metadata/PRJNA1072118_$species.tsv |
-  while read accession; do
+ mkdir -p RAW_DATA/$species links/$species
+ cut -f 1,7 metadata/PRJNA1072118_$species.tsv |
+  while read accession library; do
    fasterq-dump --seq-defline '@$sn[_$rn]/$ri'\
     --split-files $accession\
     --outdir RAW_DATA/$species;
+   # Create links to have files with correct names
+   ln -s RAW_DATA/$species/${accession}_1.fastq links/$species/${library}_1.fastq
+   ln -s RAW_DATA/$species/${accession}_2.fastq links/$species/${library}_2.fastq
  done
+ 
+ 
 done
 
