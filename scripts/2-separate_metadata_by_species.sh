21eb4d50f05c3e92864091cc36101d928cdcd7a4
KarenGoncalves <cris.kgs@gmail.com>
2024-02-19 10:13:24 -0500

    corrected script separating species metadata

--- scripts/2-separate_metadata_by_species.sh
+++ scripts/2-separate_metadata_by_species.sh
@@ -1,6 +1,6 @@
 # Separate metadata file into arabidopsis and poplar
 awk -F '\t' -v OFS='\t' 'NR == 1 {print} NR > 1\
- && $6 ~ /P.+/ {print}'\
+ && $6 ~ /^P.+/ {print}'\
  metadata/PRJNA1072118.tsv >\
  metadata/PRJNA1072118_Poplar.tsv
 
