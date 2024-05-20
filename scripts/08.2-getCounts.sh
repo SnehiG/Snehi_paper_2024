9939023a6ad611c073b4fbac1d3b7ecb5c756157
Karen Cristine Gonçalves <cris.kgs@gmail.com>
2024-05-20 10:13:20 -0400

    Update 08.2-getCounts.sh
    correcting folder names

--- scripts/08.2-getCounts.sh
+++ scripts/08.2-getCounts.sh
@@ -1,11 +1,11 @@
 # use R and bioconductor to get read counts for each species
-mkdir counts
+
 
 for SPECIES in Arabidopsis Poplar;
-	outputDIR=./counts
-	
-	TxDbPath=./${SPECIES}_TxDb.RData
-	bam_baiPath=./alignments/${SPECIES}/alignments
+	outputDIR=./$SPECIES/counts
+	mkdir $outputDIR
+	TxDbPath=./$SPECIES/${SPECIES}_TxDb.RData
+	bam_baiPath=./$SPECIES/alignments
 	
 	Rscript $DIR/scripts/getcounts.R\
 	 ${bam_baiPath}\
